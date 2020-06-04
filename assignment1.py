b93ce102c7b14f78ac37ae29fd48e1de480671bb
Yee <40756770+Grindewald1900@users.noreply.github.com>
2020-06-04 16:21:54 -0400

    Update assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -5,11 +5,10 @@ import pylab
 import threading
 import time
 import random
+import numpy as np
 from pynput import keyboard,mouse
 from matplotlib.widgets import Button
 from pynput.mouse import Controller, Listener
-
-
 
 path = 'D:\Desktop\CS516\Assignment1\images'
 docs = ('\\t1.nii','\\t2.nii','\\tof.nii','\swi.nii','\\bold.nii')
@@ -21,7 +20,7 @@ size = (0,0,0)
 event_count = 0
 
 def viewer(brain,slice,view):
-    """ Function to show the slice from 3d image"""
+    """ Function to show the slice from 3d image (part 1)"""
     # For test use
     # time.sleep(0.5)
     fig = plt.figure("Assignment1")
@@ -36,6 +35,63 @@ def viewer(brain,slice,view):
     plt.ion()
     plt.pause(0.1)
     plt.show()
+
+def viewer2(brain,slice,view):
+    """ Function to show the simple 2d FFT (part 2a)"""
+    fig = plt.figure("Assignment1")
+    plt.subplot(1,2,1)
+    plt.imshow(brain[:,:,slice],cmap=style)
+    plt.title("Slice = "+ str(slice)+"\nView = "+ view)
+    plt.subplot(1,2,2)
+    plt.imshow(my_fft(brain[:,:,slice]))
+    plt.title("requence domain of raw")
+    # Interactive mode
+    plt.ion()
+    plt.pause(0.1)
+    plt.show()
+
+def viewer3(brain,slice,view):
+    """ Function to show the gaussian filter (part 2b)"""
+    raw = brain[:,:,slice]
+    filter(my_fft(raw),raw)
+
+def viewer4(brain,slice,view):
+    """ Function to show all plots (part 2c)"""
+    fig = plt.figure("Assignment1")
+    plt.subplot(2,2,1)
+    plt.imshow(brain[:,:,slice],cmap=style)
+    plt.title("raw image")
+    plt.subplot(2,2,2)
+    plt.imshow(my_fft(brain[:,:,slice]))
+    plt.title("2d fft")
+    plt.subplot(2,2,3)
+    plt.show()
+
+
+def my_fft(img):
+    fft = np.fft.fft2(img)
+    center = np.fft.fftshift(fft)
+    return np.log(1+np.abs(center))
+
+def filter(rotim,raw):
+    sz_x = rotim.shape[0]
+    sz_y = rotim.shape[1]
+    [X, Y] = np.mgrid[0:sz_x, 0:sz_y]
+    xpr = X - int(sz_x) // 2
+    ypr = Y - int(sz_y) // 2
+    count=1
+    for sigma in range(1,25,5):
+        gaussfilt = np.exp(-((xpr**2+ypr**2)/(2*sigma**2)))/(2*np.pi*sigma**2)
+        fft = np.fft.fft2(raw)
+        ifft = np.fft.ifft2(gaussfilt*fft)
+        log_ifft = np.log(1+np.abs(ifft))
+        plt.subplot(1,5,count)
+        plt.imshow(log_ifft)
+        plt.title('sigma='+str(sigma))
+        plt.ion()
+        plt.pause(0.01)
+        plt.show()
+        count =count + 1
 
 def on_release(key):
     """ Function to listen on mouse scroll"""
@@ -57,7 +113,8 @@ def on_scroll(x,y,dx,dy):
         slice_id = 0
     if slice_id < 0:
         slice_id = size[2]-1
-    viewer(brain,slice_id,view)
+    #change the function here for different part of assignment
+    viewer3(brain,slice_id,view)
     # print("Thread id==>%s, Thread num==> %d\n"%(threading.current_thread().name,threading.activeCount()))
 
 def button_change_color():
@@ -77,19 +134,18 @@ def listener():
         listener.join()
     # with Listener(on_release=on_release) as listener:
     #     listener.join()
-mouse = Controller
-data = nib.load(path+docs[0])
-brain = data.get_fdata()
-size = brain.shape
-listener()
+
+if __name__ == '__main__':
+    mouse = Controller
+    data = nib.load(path+docs[0])
+    brain = data.get_fdata()
+    size = brain.shape
+    # viewer2(brain,slice_id,view)
+    listener()
+
 # mouse.scroll(0,1)
 
 # For test use
 # for i in range(50):
 #     viewer2(brain,slice_id+i,1)
 #     print(slice_id+i)
-
-
-
-
-
